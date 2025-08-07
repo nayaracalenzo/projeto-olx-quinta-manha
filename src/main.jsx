5270a2c3ed9ef30b4365de4b13e9ad3c1bb80154
Rodrigo Medeiros <dev.rodrigomedeiros@gmail.com>
2025-08-07 11:48:27 -0300

    primeiro commit

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./pages/App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Cadastro from "./pages/Cadastro.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<App />} />
+        <Route path={"/cadastro"} element={<Cadastro />} />
+        <Route path={"/dashboard"} element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import BodyList from "../components/BodyList";
+import Header from "../components/Header";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <Header />
+      <BodyList />
+    </div>
+  );
+}
