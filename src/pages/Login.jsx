4df2b1f9a236271acf84cea9403ad805b98de545
Rodrigo Medeiros <dev.rodrigomedeiros@gmail.com>
2025-09-04 08:57:42 -0300

    trabalhando o fetch login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,28 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Login() {
+  const [dataLogin, setDataLogin] = useState({
+    email: "",
+    senha: "",
+  });
+
+  function handleChangeLogin(event) {
+    const { name, value } = event.target;
+
+    setDataLogin((prevDataLogin) => {
+      return { ...prevDataLogin, [name]: value };
+    });
+  }
+
+  function handleSubmitLogin(event) {
+    event.preventDefault();
+
+    console.log(dataLogin);
+
+    
+  }
+
   return (
     <main className="w-full h-screen flex flex-col items-center justify-center px-4 ">
       <div className="max-w-sm w-full text-gray-600 space-y-8">
@@ -25,12 +47,14 @@ export default function Login() {
             </p>
           </div>
         </div>
-        <form onSubmit={(e) => e.preventDefault()}>
+        <form onSubmit={handleSubmitLogin}>
           <div className="mb-3">
             <label className="font-medium">Email</label>
             <input
               type="email"
               name="email"
+              onChange={handleChangeLogin}
+              value={dataLogin.email}
               required
               className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
             />
@@ -40,6 +64,8 @@ export default function Login() {
             <input
               type="password"
               name="senha"
+              onChange={handleChangeLogin}
+              value={dataLogin.senha}
               required
               className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
             />
